f032ab9548bd0f1c4194eac8eddff36ba8b3d089
Ibtid <ibtid011@example.com>
2020-10-11 00:11:50 +0600

    Sidebar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import NavBar from "./components/Navbar/NavBar"
+
 import {BrowserRouter as Router} from 'react-router-dom'
 
 import './App.css';
+import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
   return (
     <Router>
+      <Sidebar/>
       <NavBar/>
     </Router>
   );
---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import {FaTimes} from 'react-icons/fa'
+import {Link as LinkS} from "react-scroll"
+import {Link} from "react-router-dom"
+import "./Sidebar.css"
+
+const Sidebar = () => {
+    return (
+        <aside className="SidebarContainer isOpen">
+            <div className="Icon">
+               <FaTimes className="CloseIcon"/> 
+            </div>  
+            <div className="SidebarWrapper">
+                <div className="SidebarMenu">
+                    <LinkS to="about" className="SidebarLink">
+                        About
+                    </LinkS>
+                    <LinkS to="discover" className="SidebarLink">
+                        Discover
+                    </LinkS>
+                    <LinkS to="services" className="SidebarLink">
+                        Services
+                    </LinkS>
+                    <LinkS to="signup" className="SidebarLink">
+                        Sign Up
+                    </LinkS>
+                    <div className="SideBtnWrap">
+                        <Link to="/signip" className="SidebarRoute">Sign In</Link>
+                    </div>
+                </div>
+            </div>          
+        </aside>
+    )
+}
+
+export default Sidebar

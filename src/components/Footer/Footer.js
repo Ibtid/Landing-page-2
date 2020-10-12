1a2217725a491ebefc7e71373fd21eed9baa571b
Ibtid <ibtid011@example.com>
2020-10-13 01:29:02 +0600

    do it

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./Footer.css"
+
+const Footer = () => {
+    return (
+        <div className="FooterContainer">
+            <div className="FooterWrap">
+                <div className="FooterLinksContainer">
+                    <div className="FooterLinksWrapper">
+                        
+                    </div>
+                </div>
+            </div>            
+        </div>
+    )
+}
+
+export default Footer
